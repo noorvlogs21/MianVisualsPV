836d07e0c63c062caeef669e54bf96ad5fcfd179
Noor Uddin <noorvlogs21@gmail.com>
2018-12-10 17:39:05 +0000

    updated login page

--- js/PVJS/login.js
+++ js/PVJS/login.js
@@ -2,22 +2,21 @@
 $('#regSection').hide(); // hide the register form
 $('#loginForm').hide(); // hide the login button when on login form
 
-$( "#regForm" ).click(function() {
-    $('#loginSection').hide();
-    $('#regForm').hide();
-    $('#regSection').fadeIn();
-    $('#loginForm').fadeIn();
-  });
+$("#regForm").click(function () {
+  $('#loginSection').hide();
+  $('#regForm').hide();
+  $('#regSection').fadeIn();
+  $('#loginForm').fadeIn();
+});
 
-$( "#loginForm" ).click(function() {
-    $('#regSection').hide();
-    $('#loginForm').hide();
-    $('#loginSection').fadeIn();
-    $('#regForm').fadeIn();
-  });
+$("#loginForm").click(function () {
+  $('#regSection').hide();
+  $('#loginForm').hide();
+  $('#loginSection').fadeIn();
+  $('#regForm').fadeIn();
+});
 
-  //matINIT - tooltips
-  $(document).ready(function(){
-    $('.tooltipped').tooltip();
-  });
-        
+//matINIT - tooltips
+$(document).ready(function () {
+  $('.tooltipped').tooltip();
+});
